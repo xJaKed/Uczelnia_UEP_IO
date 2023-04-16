c47e29019ba73730cb5e7ec2d5a0bb7dff776861
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-04-16 17:45:53 +0200

    Circle added

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -16,4 +16,14 @@ a_romb=10
 h_romb=5
 pole_rombu = int(a_romb*h_romb)
 obwod_rombu = a*4
-print("Obwod trojkata wynosi " + str(obwod_rombu) + ", zas pole wynosi " + str(pole_rombu) + ".")
+print("Obwod rombu wynosi " + str(obwod_rombu) + ", zas pole wynosi " + str(pole_rombu) + ".")
+
+# kolo
+import math
+
+
+r_kolo = 5
+pole_kolo = float(math.pi)*(r_kolo)**2
+obwod_kolo = 2*float(math.pi)*r_kolo
+print("Obwod kola wynosi " + str(obwod_kolo) + ", zas pole wynosi " + str(pole_kolo) + ".")
+
