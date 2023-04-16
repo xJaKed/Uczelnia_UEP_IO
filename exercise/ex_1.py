a5371f5e2a03d32678a36869797835621b99780b
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-04-16 16:47:47 +0200

    Changed name2

---
+++ exercise/ex_1.py
@@ -0,0 +1,12 @@
+def hello(a: str) -> str:
+    # TODO
+    return "Hello"
+
+
+print("Example")
+print(hello("Kuba"))
+
+assert hello("Kuba") == "Hello Kuba"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
