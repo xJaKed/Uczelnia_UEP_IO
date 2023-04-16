262311f058bc2df313747b203556ec8e6c667395
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-04-16 17:32:51 +0200

    Rhombus added

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -9,3 +9,11 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynosi " + str(pole) + ".")
+
+# romb
+
+a_romb=10
+h_romb=5
+pole_rombu = int(a_romb*h_romb)
+obwod_rombu = a*4
+print("Obwod trojkata wynosi " + str(obwod_rombu) + ", zas pole wynosi " + str(pole_rombu) + ".")
