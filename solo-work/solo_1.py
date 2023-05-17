eac62ee1539eaf45e98cc8e5a6bbc083b45ca71f
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-05-17 20:07:07 +0200

    Zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -10,3 +10,10 @@ print("{} {}".format(hello, student))
 student = input("Wpisz swoje imie")
 
 print("Hello {}".format(student))
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi: {}".format(liczba_studentow))
